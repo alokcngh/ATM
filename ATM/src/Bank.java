e0274ebf9079331f33750f2f2547fa13ab156d25
Administrator <Administrator@IK-PC>
2022-04-19 19:43:08 +0530

    fuy

--- ATM/src/Bank.java
+++ ATM/src/Bank.java
@@ -1,9 +1,46 @@
 
 public class Bank {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private String name;
+	
+	private ArrayList<User> users;
+	
+	private ArrayList<Account> accounts; 
+	
+	public String getNewUserUUID(){
+	
+		//init
+		String uuid;
+		Random rng= new Random();
+		int len=6;
+		boolean nonUnique;
+		
+		//continue looping untill we get a unique ID
+		do{
+			
+			//generate the number
+			uuid= "";
+			for(int c=0;c<len;c++){
+				uuid +=(Integer)rng.nextInt(10).toString();
+			}
+			
+			//check to make sure it's unique
+			nonUnique= false;
+			for(User u : this.users){
+				if(uuid.compareTo(u.getUUID())==0){
+					nonUnique=true;
+					break;
+				}
+			}
+			
+		}while(nonUnique);
+		
+		return uuid;
 	}
 
+	public String getNewAccountUUID(){
+		
+	}
+
+	
 }
--- ATM/src/User.java
+++ ATM/src/User.java
@@ -1,9 +1,83 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class User {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	/**
+	 *The first name of the user. 
+	 */
+	private String firstName;
+	
+	/**
+	 *The first name of the user. 
+	 */
+	private String lastName;
+	
+	/**
+	 *The ID number of the user. 
+	 */
+	private String uuid;
+	
+	/**
+	 *The MDS hash of the user's pin number. 
+	 */
+	private String pinHash[];
+	
+	/**
+	 *The list of accounts for this user. 
+	 */
+	private ArrayList<Account> accounts;
+	
+	/** 
+	 *create a new user
+	 *@param firstName the user's first name
+	 * @param lastName the user's last name
+	 * @param pin      the user's account pin number
+	 * @param theBank  the Bank object that the user is a customer of
+	 */
+	public User(String firstName, String lastName, String pin, Bank the Bank){
+		
+		//set user's name
+		this.firstName= firstName;
+		this.lastName= lastName;
+		
+		//store the pin's MDS hash, rather than the original value, for
+		//security reasons
+		try{
+		MessageDigest md=MessageDigest.getInstance("MDS");
+		this.pinHash= md.digest(pin.getBytes());
+		}
+		catch(NoSuchAlgorithmException e){
+			System.err.println("error, caught NoSuchAlgorithmException ");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		//get a new, unique universal ID for the user
+		this.uuid = theBank.getNewUserUUID();
+		
+		//create empty list of accounts
+		this.accounts = new ArrayList<Account>();
+		
+		//print log message
+		System.out.println("New user %s, %s with ID %s created. \n",lastName,firstName, this.uuid);
 	}
-
-}
+	
+	/**
+	 * Add an account for the user
+	 * @param anAcct the account to add 
+	  */
+	public void addAccount(Account anAcct) {
+		this.accounts.add(anAcct);
+		
+	}
+	
+	/**
+	 * Return the user's ID
+	 * @return the uuid
+	 */
+	public String getUUID(){
+		return this.uuid;
+	}
+	}
